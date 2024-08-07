c7e56107d85c6aaa9e7dd7c9368edc49152718aa
Yining Zhang <106020817+ynzhang23@users.noreply.github.com>
2024-08-07 16:04:27 +0800

    Add nav bar component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import { Sidebar } from "./_components/sidebar"
 import { OrgSidebar } from "./_components/org-sidebar"
+import { Navbar } from "./_components/navbar";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
@@ -15,6 +16,7 @@ const DashboardLayout = ({
         <div className="flex gap-x-3 h-full">
           <OrgSidebar />
           <div className="h-full flex-1">
+            <Navbar />
             {children}
           </div>
         </div>
