c1d05963a6d4bb27a61b832f3f80e32965844ca2
Yining Zhang <106020817+ynzhang23@users.noreply.github.com>
2024-08-07 18:09:13 +0800

    Add organization features to sidebar
    Create button for new organization
    Create button to switch between organizations
    Add floating hint message box

---
+++ app/(dashboard)/_components/sidebar/item.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Image from "next/image";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+import { cn } from "@/lib/utils";
+import { Hint } from "@/components/hint";
+
+interface ItemProps {
+  id: string;
+  title: string;
+  imageUrl: string;
+}
+
+export const Item = ({ id, title, imageUrl }: ItemProps) => {
+  const { organization } = useOrganization();
+  const { setActive } = useOrganizationList();
+  const isActive = organization?.id === id;
+
+  const onClick = () => {
+    if (!setActive) return;
+    setActive({ organization: id });
+  };
+
+  return (
+    <div className="aspect-square relative">
+      <Hint
+        label={title}
+        side="right"
+        align="start"
+        sideOffset={18}
+      >
+        <Image
+          fill
+          alt={title}
+          src={imageUrl}
+          onClick={onClick}
+          className={cn(
+            "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
+            isActive && "opacity-100"
+          )}
+        />
+      </Hint>
+    </div>
+  );
+};
